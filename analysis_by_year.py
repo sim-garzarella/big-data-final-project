cc6c9a11b66435d005d90c02d87350f63c66cad0
sim-garzarella <simonegarz@hotmail.it>
2021-09-13 19:50:31 +0200

    Added the total number of titles and the information about isAdult

--- analysis_by_year.py
+++ analysis_by_year.py
@@ -15,11 +15,11 @@ parser.add_argument("--output_path", type=str, help="Output folder")
 
 # parse arguments
 args = parser.parse_args()
-#input_filepath_1 = args.input_path_1
-#input_filepath_2 = args.input_path_2
+# input_filepath_1 = args.input_path_1
+# input_filepath_2 = args.input_path_2
 input_filepath_3 = args.input_path_3
-#input_filepath_4 = args.input_path_4
-#input_filepath_5 = args.input_path_5
+# input_filepath_4 = args.input_path_4
+# input_filepath_5 = args.input_path_5
 output_filepath = args.output_path
 
 # initialize SparkSession
@@ -42,22 +42,35 @@ def element_count(input_list):
     return element_to_count
 
 
+def total_count(input_map):
+    tot = 0
+
+    for element in input_map:
+        tot += input_map[element]
+
+    return str(tot)
+
+
 # name_basics = sc.textFile(input_filepath_1).cache().map(lambda line: line.strip().split('\t'))
 # title_akas = sc.textFile(input_filepath_2).cache().map(lambda line: line.strip().split('\t'))
 title_basics = sc.textFile(input_filepath_3).cache().map(lambda line: line.strip().split('\t'))
 # title_principals = sc.textFile(input_filepath_4).cache().map(lambda line: line.strip().split('\t'))
 # title_ratings = sc.textFile(input_filepath_5).cache().map(lambda line: line.strip().split('\t'))
 
-year_types_genres = title_basics.map(lambda line: ((line[5]), (line[1], line[8])))
+year_types_genres_isAdult = title_basics.map(lambda line: ((line[5]), (line[1], line[8], line[4])))
 
-year_all_types = year_types_genres.map(lambda line: ((line[0]), line[1][0])).reduceByKey(lambda a, b: a + "," + b)
+year_all_types = year_types_genres_isAdult.map(lambda line: ((line[0]), line[1][0])).reduceByKey(lambda a, b: a + "," + b)
 
-year_all_genres = year_types_genres.map(lambda line: ((line[0]), line[1][1])).reduceByKey(lambda a, b: a + "," + b)
+year_all_genres = year_types_genres_isAdult.map(lambda line: ((line[0]), line[1][1])).reduceByKey(lambda a, b: a + "," + b)
 
-year_types_count = year_all_types.map(lambda line: (("Year: " + line[0]), ("Types: ", element_count(line[1].split(",")))))
+year_isAdult = year_types_genres_isAdult.map(lambda line: ((line[0]), line[1][2])).reduceByKey(lambda a, b: a + "," + b)
+
+year_types_count = year_all_types.map(lambda line: (("Year: " + line[0]), ("Total: " + total_count(element_count(line[1].split(","))), "Types: ", element_count(line[1].split(",")))))
 
 year_genre_count = year_all_genres.map(lambda line: (("Year: " + line[0]), ("Genres: ", element_count(line[1].split(",")))))
 
-year_genre_types_count = year_types_count.join(year_genre_count)
+year_isAdult_count = year_isAdult.map(lambda line: (("Year: " + line[0]), ("Is Adult: ", element_count(line[1].split(",")))))
+
+year_genre_types_count = year_types_count.join(year_genre_count).join(year_isAdult_count)
 
 year_genre_types_count.sortBy(keyfunc=lambda x: x[0], ascending=True).saveAsTextFile(output_filepath)
